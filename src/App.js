67ab0356dc8d497696208ec4dfbfdf8bbca90476
MfalconP <maryushp@gmail.com>
2023-10-22 00:25:56 +0200

    feat(dev_react) added routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import HomePage from "./pages/HomePage";
+import ItemPage from "./pages/HomePage";
 
 function App() {
   return (
-    <>
+      <>
       <Header/>
-    </>
+          <Routes>
+              <Route element={<HomePage/>} path="/"/>
+              <Route element={<ItemPage/>} path="/item"/>
+          </Routes>
+      </>
   );
 }
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM, {createRoot} from 'react-dom/client';
+import {BrowserRouter as Router} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import './styles/index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <App/>
+const root = document.getElementById('root');
+const rootReact = createRoot(root);
+rootReact.render(
+    <Router>
+        <App/>
+    </Router>
 );
