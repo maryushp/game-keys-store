98da24f52df9898a431967c430d2030219cb174a
MfalconP <maryushp@gmail.com>
2023-10-23 17:04:16 +0200

    fix(dev_react) renamed fetch methods

--- src/utils/getItemByIdAPI.js
+++ src/utils/getItemByIdAPI.js
@@ -1,8 +1,8 @@
-export const getItemById = async (id) => {
+export const getItemByIdAPI = async (id) => {
     try {
         const response = await fetch(`http://localhost:8080/item/${id}`);
         if (!response.ok) {
-            throw new Error('Failed to fetch items');
+            throw new Error('Failed to fetch item');
         }
         return await response.json();
     } catch (error) {
