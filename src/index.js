5bfee7c398f6fda59528c12a1f119016da744648
MfalconP <maryushp@gmail.com>
2023-10-26 15:02:16 +0200

    feat(dev_react) added interceptors and their configuration

--- src/index.js
+++ src/index.js
@@ -4,9 +4,11 @@ import {BrowserRouter as Router} from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.css';
 import App from './App';
 import './styles/index.css';
+import FetchInterceptors from "./utils/InterceptorsConfig";
 
 const root = document.getElementById('root');
 const rootReact = createRoot(root);
+const interceptors = new FetchInterceptors();
 rootReact.render(
     <Router>
         <App/>
---
+++ src/utils/InterceptorsConfig.js
@@ -0,0 +1,39 @@
+import fetchIntercept from 'fetch-intercept';
+import {getCookie, removeCookie} from './CookiesManager'
+
+class FetchInterceptors {
+    constructor() {
+        this.setupRequestInterceptor();
+        this.setupResponseInterceptor();
+    }
+
+    setupRequestInterceptor() {
+        fetchIntercept.register({
+            request: (url, config) => {
+                const token = getCookie('token');
+                if (token) {
+                    if (!config) {
+                        config = {};
+                    }
+                    config.headers = config.headers || new Headers();
+                    config.headers.set('Authorization', `Bearer ${token}`);
+                }
+                return [url, config];
+            },
+        });
+    }
+
+    setupResponseInterceptor() {
+        fetchIntercept.register({
+            response: (response) => {
+                if (response.status === 401) {
+                    removeCookie('token');
+                    localStorage.removeItem('userData');
+                }
+                return response;
+            },
+        });
+    }
+}
+
+export default FetchInterceptors;
