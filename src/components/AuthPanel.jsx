e6226ae4a6399e751bc72e5320d9cd7d4032a414
MfalconP <maryushp@gmail.com>
2023-10-30 13:02:43 +0100

    fix(dev_react) refactored auth panel

--- src/components/AuthPanel.jsx
+++ src/components/AuthPanel.jsx
@@ -96,20 +96,16 @@ const AuthPanel = () => {
         if (signUpPass.length < 8) {
             setErrorText("Password should have at least 8 chars!");
             setSignUpError(true);
-        }
-        else if (!signUpEmail.includes("@")) {
+        } else if (!signUpEmail.includes("@")) {
             setErrorText("Enter a valid email!");
             setSignUpError(true);
-        }
-        else if (signUpName === "") {
+        } else if (signUpName === "") {
             setErrorText("Name cannot be empty!");
             setSignUpError(true);
-        }
-        else if (signUpSurname === ""){
+        } else if (signUpSurname === "") {
             setErrorText("Surname cannot be empty!");
             setSignUpError(true);
-        }
-        else
+        } else
             handleRegister()
     }
 
@@ -119,94 +115,126 @@ const AuthPanel = () => {
                 handleData(data);
                 getUser(data.id)
                     .then((data) => handleUserData(data))
-                    .catch(() => {setSignUpError(true); setErrorText("Try another credentials!")})
+                    .catch(() => {
+                        setSignUpError(true);
+                        setErrorText("Try another credentials!")
+                    })
                     .finally(() => {
                         if (!signUpError) {
                             setTimeout(() => window.location.reload(), 100)
                         }
                     });
             })
-            .catch(() => {setSignUpError(true); setErrorText("Try another credentials!")})
+            .catch(() => {
+                setSignUpError(true);
+                setErrorText("Try another credentials!")
+            })
             .finally();
     }
 
     return (
         <div className={`d-lg-flex d-none flex-column position-absolute gap-3 p-5 auth-panel shadow-lg`}>
 
-            { localStorage.getItem('userData') ?
-                (   <>
-                        <h1 className="text-white text-center">Welcome,</h1>
+            {localStorage.getItem('userData') ?
+                (<>
+                        <h2 className="text-white text-center">Welcome,</h2>
                         <div className="d-flex flex-wrap gap-2">
-                            <h3 className="text-white text-center">{JSON.parse(localStorage.getItem('userData')).name}</h3>
-                            <h3 className="text-white text-center">{JSON.parse(localStorage.getItem('userData')).surname}</h3>
+                            <h4 className="text-white text-center">{JSON.parse(localStorage.getItem('userData')).name}</h4>
+                            <h4 className="text-white text-center">{JSON.parse(localStorage.getItem('userData')).surname}</h4>
                         </div>
-                        <Button variant="danger" className="rounded-5" onClick={handleSignOut}>Sign Out</Button>
+                        <h5 className="text-white text-center">{JSON.parse(localStorage.getItem('userData')).email}</h5>
+                        <div className="d-flex flex-wrap gap-5 my-5">
+                            <Button variant="info" className="rounded-5 fw-bolder">My orders</Button>
+                            <Button variant="danger" className="rounded-5 fw-bolder" onClick={handleSignOut}>Sign
+                                Out</Button>
+                        </div>
                     </>
                 )
                 :
                 (<>
-                        <div className="d-flex flex-wrap gap-5">
-                            <Button variant={signInIsClicked ? `info` : `outline-light`} className={signInIsClicked ? `disabled mx-3 rounded-5 fw-bolder`: `mx-3 rounded-5 fw-bolder`} onClick={toggleSignInOption}>SIGN IN</Button>
-                            <Button variant={signUpIsClicked ? `info` : `outline-light`} className={signUpIsClicked ? `disabled mx-3 rounded-5 fw-bolder`: `mx-3 rounded-5 fw-bolder`} onClick={toggleSignUpOption}>SIGN UP</Button>
-                        </div>
+                    <div className="d-flex flex-wrap gap-5">
+                        <Button variant={signInIsClicked ? `info` : `outline-light`}
+                                className={signInIsClicked ? `disabled mx-3 rounded-5 fw-bolder` : `mx-3 rounded-5 fw-bolder`}
+                                onClick={toggleSignInOption}>SIGN IN</Button>
+                        <Button variant={signUpIsClicked ? `info` : `outline-light`}
+                                className={signUpIsClicked ? `disabled mx-3 rounded-5 fw-bolder` : `mx-3 rounded-5 fw-bolder`}
+                                onClick={toggleSignUpOption}>SIGN UP</Button>
+                    </div>
 
-                        <div className="d-flex flex-column align-items-center my-5 p-3">
-                            {signInIsClicked ?
-                                (
-                                    <div className="d-flex flex-column align-items-center">
-                                        <h3 className="text-white text-center">Email</h3>
-                                        <Input placeholder="Email" innerRef={inputRefEmailSignIn} onChange={(e) => {setSignInEmail(e.target.value); setError(false)}} className="rounded-5 userDataInput my-3"/>
-                                        <h3 className="text-white text-center">Password</h3>
-                                        <Input type="password" innerRef={inputRefPassSignIn} onChange={(e) => {setSignInPassword((e.target.value)); setError(false)}} placeholder="Password" className="rounded-5 userDataInput my-3"/>
-                                        <Button variant="success"
-                                                className="my-5 w-50 rounded-5"
-                                                onClick={handleSignIn}>SUBMIT</Button>
-                                        {error ? <h4 className="text-danger text-center">Wrong credentials!</h4> : <></>}
+                    <div className="d-flex flex-column align-items-center my-5 p-3">
+                        {signInIsClicked ?
+                            (
+                                <div className="d-flex flex-column align-items-center">
+                                    <h3 className="text-white text-center">Email</h3>
+                                    <Input placeholder="Email" innerRef={inputRefEmailSignIn} onChange={(e) => {
+                                        setSignInEmail(e.target.value);
+                                        setError(false)
+                                    }} className="rounded-5 userDataInput my-3"/>
+                                    <h3 className="text-white text-center">Password</h3>
+                                    <Input type="password" innerRef={inputRefPassSignIn} onChange={(e) => {
+                                        setSignInPassword((e.target.value));
+                                        setError(false)
+                                    }} placeholder="Password" className="rounded-5 userDataInput my-3"/>
+                                    <Button variant="success"
+                                            className="my-5 w-50 rounded-5"
+                                            onClick={handleSignIn}>SUBMIT</Button>
+                                    {error ? <h4 className="text-danger text-center">Wrong credentials!</h4> : <></>}
 
-                                    </div>
-                                )
-                                :
-                                (
-                                    <div className="d-flex flex-column align-items-center">
-                                        <h3 className="text-white text-center">Name</h3>
-                                        <Input placeholder="Name"
-                                               innerRef={inputRefNameSignUp}
-                                               className="rounded-5 userDataInput my-3"
-                                               onChange={(e) => {setSignUpName(e.target.value); setSignUpError(false)}}
-                                        />
+                                </div>
+                            )
+                            :
+                            (
+                                <div className="d-flex flex-column align-items-center">
+                                    <h3 className="text-white text-center">Name</h3>
+                                    <Input placeholder="Name"
+                                           innerRef={inputRefNameSignUp}
+                                           className="rounded-5 userDataInput my-3"
+                                           onChange={(e) => {
+                                               setSignUpName(e.target.value);
+                                               setSignUpError(false)
+                                           }}
+                                    />
 
-                                        <h3 className="text-white text-center">Surname</h3>
-                                        <Input placeholder="Surname"
-                                               innerRef={inputRefSurnameSignUp}
-                                               className="rounded-5 userDataInput my-3"
-                                               onChange={(e) => {setSignUpSurname(e.target.value); setSignUpError(false)}}
-                                        />
+                                    <h3 className="text-white text-center">Surname</h3>
+                                    <Input placeholder="Surname"
+                                           innerRef={inputRefSurnameSignUp}
+                                           className="rounded-5 userDataInput my-3"
+                                           onChange={(e) => {
+                                               setSignUpSurname(e.target.value);
+                                               setSignUpError(false)
+                                           }}
+                                    />
 
-                                        <h3 className="text-white text-center">Email</h3>
-                                        <Input placeholder="Email"
-                                               innerRef={inputRefEmailSignUp}
-                                               className="rounded-5 userDataInput my-3"
-                                               type = "email"
-                                               onChange={(e) => {setSignUpEmail(e.target.value); setSignUpError(false)}}
-                                        />
+                                    <h3 className="text-white text-center">Email</h3>
+                                    <Input placeholder="Email"
+                                           innerRef={inputRefEmailSignUp}
+                                           className="rounded-5 userDataInput my-3"
+                                           type="email"
+                                           onChange={(e) => {
+                                               setSignUpEmail(e.target.value);
+                                               setSignUpError(false)
+                                           }}
+                                    />
 
-                                        <h3 className="text-white text-center">Password</h3>
-                                        <Input type="password"
-                                               innerRef={inputRefPassSignUp}
-                                               placeholder="Password"
-                                               className="rounded-5 userDataInput my-3"
-                                               onChange={(e) => {setSignUpPass(e.target.value); setSignUpError(false)}}/>
+                                    <h3 className="text-white text-center">Password</h3>
+                                    <Input type="password"
+                                           innerRef={inputRefPassSignUp}
+                                           placeholder="Password"
+                                           className="rounded-5 userDataInput my-3"
+                                           onChange={(e) => {
+                                               setSignUpPass(e.target.value);
+                                               setSignUpError(false)
+                                           }}/>
 
-                                        <Button variant="success" className="my-3 w-50 rounded-5" onClick={handleSignUp}>SUBMIT</Button>
-                                        {signUpError ? <h4 className="text-danger text-center">{errorText}</h4> : <></>}
-                                    </div>
-                                )
-                            }
-                        </div>
-                    </>)
+                                    <Button variant="success" className="my-3 w-50 rounded-5"
+                                            onClick={handleSignUp}>SUBMIT</Button>
+                                    {signUpError ? <h4 className="text-danger text-center">{errorText}</h4> : <></>}
+                                </div>
+                            )
+                        }
+                    </div>
+                </>)
             }
-
-
 
 
         </div>
