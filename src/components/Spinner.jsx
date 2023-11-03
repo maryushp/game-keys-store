b02970c984b8423ff303f050a66369ee1ba5c6e5
MfalconP <maryushp@gmail.com>
2023-11-03 17:49:58 +0100

    fix(dev_react) introduced separate component spinner

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,12 @@
+import "../styles/Spinner.css"
+import {ClipLoader} from "react-spinners";
+import React from "react";
+
+const Spinner = () => {
+    return (
+        <div className="d-flex flex-wrap justify-content-center default-spinner">
+            <ClipLoader color="#ffffff" size={60} />
+        </div>
+    );
+};
+export default Spinner;
